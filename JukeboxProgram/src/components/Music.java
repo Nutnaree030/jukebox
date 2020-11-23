0dbc00fbe5fe38094d0cb448c5ce0ccaac2e1883
Nutnaree030 <nutnaree.seesawai@mail.kmutt.ac.th>
2020-11-23 18:18:25 +0700

    edit Music

--- JukeboxProgram/src/components/Music.java
+++ JukeboxProgram/src/components/Music.java
@@ -5,7 +5,7 @@ package components;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.Scanner;
 /**
  *
  * @author LENOVO
@@ -19,6 +19,35 @@ public class Music {
         this.musicName = name;
     }
 
+    public void findMusic(){
+        Scanner a = new Scanner(System.in);
+        System.out.print("What's song you want to find : ");
+        String music1 = a.nextLine();
+        
+        if (musicName==null){
+            System.out.println("Result : Not Found"); 
+        } else if (music1==musicName) {
+            System.out.println("Result : "+ music1); /* รอใส่ตัวชื่อเพลง */
+        } else {
+            System.out.println("Error");
+        }
+        
+    }
+    public void chooseMusic(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Choose song :");
+        String music2 = input.nextLine();
+            
+        if (musicName==null){
+            System.out.println("Not Found"); /* รอใส่ตัวชื่อเพลง */
+        } else if (music2==musicName){
+           System.out.println("Now You Play : "+music2);
+        } else {
+            System.out.println("Error");
+        }
+    }
+    
+    
     @Override
     public String toString() {
         return "Music{" + "orderNumber=" + orderNumber + ", musicName=" + musicName + '}';
